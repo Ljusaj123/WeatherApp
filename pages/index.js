feb2148018d3786c549055484b6a39a85b161f01
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-02-20 18:46:52 +0100

    dodan ključ za api i baza linka

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
-
+import React from "react";
 
 export default function Home() {
+
+  const api ={
+    key: "f624f11948d4be576b2ea253b7db39b4",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
   return (
     <div className="app">
       
