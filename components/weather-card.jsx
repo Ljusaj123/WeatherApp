1ac71569c208e8cce08cb0d77881dd6c27fac4f5
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-03-06 18:56:16 +0100

    formatted

--- components/weather-card.jsx
+++ components/weather-card.jsx
@@ -1,64 +1,84 @@
 /* eslint-disable @next/next/no-img-element */
-import {MdOutlineClose} from 'react-icons/md';
+import { MdOutlineClose } from "react-icons/md";
 
-const Card =({props, funkcija})=>{
+const Card = ({ props, funkcija }) => {
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
 
-    const dateBuilder = (d) => {
-        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
-        let day = days[d.getDay()];
-        let date = d.getDate();
-        let month = months[d.getMonth()];
-        let year = d.getFullYear();
-    
-        return `${day} ${date} ${month} ${year}`
-      }
-    return (
-            <div className="card-container">
-                 <div className="image-container">
-                {props.main.temp >= 16 ? 
-                     (<img src="/warm-bg.jpg" alt="aaa" />) : 
-                    (<img src="/cold-bg.jpg" alt="aaa" />)}
-                
-                 </div> 
-        
-              <div className="text-container">
-                 <div className="close-container" onClick={e => funkcija(e)}>
-                     <MdOutlineClose/>
-                    
-                 </div>
-                 <div className="city-info">
-                     <h2>{props.name}, {props.sys.country}</h2>
-                     <h4>{dateBuilder(new Date())}</h4>
-                 </div>
-                 <div className="weather-container">
-                 <div className="weather-info-container">
-                 <p>Temeperature:</p>
-                     <p>{Math.round(props.main.temp)}&deg;</p>
-                 </div>
-                 <div className="weather-info-container">
-                 <p>Humidity:</p>
-                     <p>{props.main.humidity}</p>
-                 </div>
-                 <div className="weather-info-container">
-                     <p>Pressure:</p>
-                     <p>{props.main.pressure}</p>
-                 </div>
-                 <div className="weather-info-container">
-                     <p>Weather:</p>
-                     <p>{props.weather[0].description}</p>
-                 </div>
-                 <div className="weather-info-container">
-                     <p>Wind:</p>
-                     <p>{props.wind.speed}km/h</p>
-                 </div>
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
 
-                </div>
-             </div>
-            </div> 
-    )
+    return `${day} ${date} ${month} ${year}`;
+  };
+  return (
+    <div className="card-container">
+      <div className="image-container">
+        {props.main.temp >= 16 ? (
+          <img src="/warm-bg.jpg" alt="aaa" />
+        ) : (
+          <img src="/cold-bg.jpg" alt="aaa" />
+        )}
+      </div>
 
-}
+      <div className="text-container">
+        <div className="close-container" onClick={(e) => funkcija(e)}>
+          <MdOutlineClose />
+        </div>
+        <div className="city-info">
+          <h2>
+            {props.name}, {props.sys.country}
+          </h2>
+          <h4>{dateBuilder(new Date())}</h4>
+        </div>
+        <div className="weather-container">
+          <div className="weather-info-container">
+            <p>Temeperature:</p>
+            <p>{Math.round(props.main.temp)}&deg;</p>
+          </div>
+          <div className="weather-info-container">
+            <p>Humidity:</p>
+            <p>{props.main.humidity}</p>
+          </div>
+          <div className="weather-info-container">
+            <p>Pressure:</p>
+            <p>{props.main.pressure}</p>
+          </div>
+          <div className="weather-info-container">
+            <p>Weather:</p>
+            <p>{props.weather[0].description}</p>
+          </div>
+          <div className="weather-info-container">
+            <p>Wind:</p>
+            <p>{props.wind.speed}km/h</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
--- pages/index.js
+++ pages/index.js
@@ -2,59 +2,57 @@ import React, { useState } from "react";
 import Card from "../components/weather-card";
 
 export default function Home() {
-
-  const api ={
+  const api = {
     key: "f624f11948d4be576b2ea253b7db39b4",
-    base: "https://api.openweathermap.org/data/2.5/"
-  }
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
 
-  const [query,setQuery]=useState('');
-  const [town,setTown]=useState('');
-  const [weatherInfo,setWeatherInfo]=useState([]);
+  const [query, setQuery] = useState("");
+  const [town, setTown] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState([]);
 
-  const handleChange =(e)=>{
-    const value  = e.target.value;
+  const handleChange = (e) => {
+    const value = e.target.value;
     setQuery(value);
-  }
+  };
 
-  const seeDouble =(result)=>{
-    return (weatherInfo.find((a)=> result.name === a.name)!==undefined) ? 1: 0;
-  }
+  const seeDouble = (result) => {
+    return weatherInfo.find((a) => result.name === a.name) !== undefined
+      ? 1
+      : 0;
+  };
 
-  const removeCard =(e)=>{
-    const city=e.target.nextSibling.firstChild.innerHTML;
-    setWeatherInfo(weatherInfo.filter((weather)=> {
-      return weather.name!==city.split(',')[0];
-    }))
-  }
-      
-  const search =(e)=>{
-    const query= e.target.value;
-    if(e.key==="Enter"){
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-      .then(data=>data.json())
-      .then(result => {
-
-        if(typeof result.main == "undefined"){
-          setWeatherInfo([...weatherInfo]);
-          setTown(query + " not found");
-          setTimeout(()=> setTown(''), 4000);
-        }
-        else if(seeDouble(result)){
-          console.log("aaa");
-          setWeatherInfo([...weatherInfo]);
-          setTown(query + " already exists");
-          setTimeout(()=> setTown(''), 4000);
-
-        }
-          else{
-          setWeatherInfo([...weatherInfo, result]);
-        }
-        setQuery('');
+  const removeCard = (e) => {
+    const city = e.target.nextSibling.firstChild.innerHTML;
+    setWeatherInfo(
+      weatherInfo.filter((weather) => {
+        return weather.name !== city.split(",")[0];
       })
-    }
+    );
+  };
 
-  }
+  const search = (e) => {
+    const query = e.target.value;
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((data) => data.json())
+        .then((result) => {
+          if (typeof result.main == "undefined") {
+            setWeatherInfo([...weatherInfo]);
+            setTown(query + " not found");
+            setTimeout(() => setTown(""), 4000);
+          } else if (seeDouble(result)) {
+            console.log("aaa");
+            setWeatherInfo([...weatherInfo]);
+            setTown(query + " already exists");
+            setTimeout(() => setTown(""), 4000);
+          } else {
+            setWeatherInfo([...weatherInfo, result]);
+          }
+          setQuery("");
+        });
+    }
+  };
   return (
     <div className="app">
       <div className="app-container">
@@ -64,8 +62,8 @@ export default function Home() {
             <h3>Find out the weather in any city </h3>
           </div>
           <div className="input-container">
-            <input 
-              type="text" 
+            <input
+              type="text"
               placeholder="Search town..."
               onChange={handleChange}
               value={query}
@@ -74,15 +72,19 @@ export default function Home() {
             <p>{town}</p>
           </div>
         </div>
-           <div className="weather-container">
-               {weatherInfo.map((weather,index)=>{
-                 return(
-                  <Card props={weather} array={weatherInfo} funkcija={removeCard}key={index}/>
-
-                 )
-               })}
-          </div>
+        <div className="weather-container">
+          {weatherInfo.map((weather, index) => {
+            return (
+              <Card
+                props={weather}
+                array={weatherInfo}
+                funkcija={removeCard}
+                key={index}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
-  )
+  );
 }
